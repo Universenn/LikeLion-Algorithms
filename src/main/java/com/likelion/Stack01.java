caed61890adc151ea1214bee4b4a4a64415a8b37
Win10ft\UserK <dnwndhks98@gmail.com>
2022-10-19 11:01:42 +0900

    Stack 구현 1. push 2. pop

--- src/main/java/com/likelion/Stack01.java
+++ src/main/java/com/likelion/Stack01.java
@@ -3,6 +3,7 @@ package com.likelion;
 public class Stack01 {
     private int[] arr = new int[10000];
 
+    // point 0부터 시작
     private int pointer = 0;
     public Stack01() {
     }
@@ -11,11 +12,19 @@ public class Stack01 {
         this.arr = new int[size];
     }
 
+    // 순차적으로 arr에 담아준다.
     public void push(int value){
         this.arr[this.pointer] = value;
         this.pointer ++;
     }
     public int[] getArr(){
         return arr;
+    }
+
+    // 마지막 arr를 빼준다
+    public int pop() {
+        int value = this.arr[this.pointer-1];
+        this.pointer--;
+        return value;
     }
 }
