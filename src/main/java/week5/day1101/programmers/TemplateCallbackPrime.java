dbb2b068257e5ff1dc1d74351a402227eba39a6d
Universenn <dnwndhks98@gmail.com>
2022-11-01 11:12:44 +0900

    소수 찾기 익명내부클래스 람다 표현식 적용

--- src/main/java/week5/day1101/programmers/TemplateCallbackPrime.java
+++ src/main/java/week5/day1101/programmers/TemplateCallbackPrime.java
@@ -8,6 +8,7 @@ public class TemplateCallbackPrime {
     boolean isPrime(int num, StatementStrategy stmt) {
 
         for (int i = 2; stmt.compare(i,num); i++) {
+            System.out.print(i+", ");
             if (num % i == 0) {
                 return false;
             }
@@ -17,7 +18,17 @@ public class TemplateCallbackPrime {
 
     public static void main(String[] args) {
         TemplateCallbackPrime tcp = new TemplateCallbackPrime();
-        boolean prime = tcp.isPrime(12);
+
+        boolean prime = tcp.isPrime(17, new StatementStrategy() {
+            @Override
+            public boolean compare(int a, int b) {
+                return a * a < b;
+            }
+        });
         System.out.println(prime);
+
+        System.out.println(tcp.isPrime(13,(a,b)-> a<b));
+        System.out.println(tcp.isPrime(19,(a,b)-> a<b/2));
+        System.out.println(tcp.isPrime(17,(a,b)-> a*a<b));
     }
 }
