b11875ebd46932a6887440529fc14c0aea913459
Universenn <dnwndhks98@gmail.com>
2022-11-01 09:33:58 +0900

    프로그래머스 소수 찾기, 소수 구분 해보기 2,3,4는 구분하지 못함.

--- src/main/java/week5/day1101/programmers/PrimeNumber.java
+++ src/main/java/week5/day1101/programmers/PrimeNumber.java
@@ -7,10 +7,9 @@ public class PrimeNumber {
     }
 
     boolean isPrime(int n){
-        boolean prime = (n % 2 != 0 || n % 3 != 0);
-        if (prime) {
-            System.out.printf("%d 는 소수가 입니다. \n", n);
-        }
+        boolean prime = (n % 2 == 0 || n % 3 == 0);
+        if (prime) {}
+        else System.out.printf("%d 는 소수 입니다. \n", n);
 
         return prime;
     }
@@ -18,13 +17,16 @@ public class PrimeNumber {
     public static void main(String[] args) {
         PrimeNumber pn = new PrimeNumber();
 
-        System.out.println(pn.isPrime(23));
+        System.out.println(pn.isPrime(32));
+        for (int i = 0; i < 32; i++) {
+            System.out.println(pn.isPrime(i));
+        }
 
-        int k = 13;
-        System.out.println(k%2);
-        System.out.println(k%3);
-        if (k % 2 == 0 || k % 3 == 0) {}
-        else System.out.printf("%d 는 소수가 입니다. \n", k);
+//        int k = 13;
+//        System.out.println(k%2);
+//        System.out.println(k%3);
+//        if (k % 2 == 0 || k % 3 == 0) {}
+//        else System.out.printf("%d 는 소수가 입니다. \n", k);
 
     }
 }
