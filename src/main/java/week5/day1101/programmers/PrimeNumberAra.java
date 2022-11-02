c1560d62942867ed78bcd7afc1d8d6b34697465e
Universenn <dnwndhks98@gmail.com>
2022-11-02 09:41:32 +0900

    2~50 숫자, 2배수 제거

---
+++ src/main/java/week5/day1101/programmers/PrimeNumberAra.java
@@ -0,0 +1,21 @@
+package week5.day1101.programmers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PrimeNumberAra {
+
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        for (int i = 2; i < 50; i++) {
+            if (i % 2 != 0) {
+                arr.add(i);
+            }
+        }
+        System.out.println(arr);
+    }
+}
