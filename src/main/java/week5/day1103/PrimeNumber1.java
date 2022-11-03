60c5abd2416c450072efffc6470447e474c6834f
Universenn <dnwndhks98@gmail.com>
2022-11-03 09:25:58 +0900

    에라토스테네스 체 시작

---
+++ src/main/java/week5/day1103/PrimeNumber1.java
@@ -0,0 +1,14 @@
+package week5.day1103;
+
+public class PrimeNumber1 {
+
+//        실습1
+//        2, 2
+//        3, 4
+//        4, 6
+//        5, 8
+//        … 을 출력 해보세요 multipleOf가 100까지
+
+
+
+}
