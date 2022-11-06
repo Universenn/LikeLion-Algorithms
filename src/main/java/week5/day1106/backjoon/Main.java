0ab61222b0017e2d1d97b1b005a624106f63665a
Universenn <dnwndhks98@gmail.com>
2022-11-06 16:15:36 +0900

    backjoon 문제 풀이 기초 조건문 4

--- src/main/java/week5/day1106/backjoon/Main.java
+++ src/main/java/week5/day1106/backjoon/Main.java
@@ -6,9 +6,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
-        if (x%4==0&&x%100!=0||x%400==0) {
+        int y = sc.nextInt();
+        if (x>0 && y >0) {
             System.out.println(1);
-        }else System.out.println(0);
+        } else if (x>0 && y < 0) {
+            System.out.println(4);
+        } else if (x<0 && y < 0) {
+            System.out.println(3);
+        }else if (x<0 && y > 0) {
+            System.out.println(2);
+        }else return;
 
     }
 }
