b3608ec129b5a199412c9f1ad7dac9b4aa41ac7b
Universenn <dnwndhks98@gmail.com>
2022-11-06 16:12:12 +0900

    backjoon 문제 풀이 기초 조건문 3

--- src/main/java/week5/day1106/backjoon/Main.java
+++ src/main/java/week5/day1106/backjoon/Main.java
@@ -6,21 +6,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
-        String grade;
-
-        switch (x/10) {
-            case 10 :
-            case 9 : grade = "A";
-                break;
-            case 8 : grade = "B";
-                break;
-            case 7 : grade = "c";
-                break;
-            case 6 : grade = "D";
-                break;
-            default: grade = "F";
-        }
-        System.out.println(grade);
+        if (x%4==0&&x%100!=0||x%400==0) {
+            System.out.println(1);
+        }else System.out.println(0);
 
     }
 }
