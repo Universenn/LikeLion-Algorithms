4e9fdc7c264ca7b8774c96fce7f6e36ed1596cf4
Universenn <dnwndhks98@gmail.com>
2022-11-07 09:39:19 +0900

    programmers 하샤드 수 구하기 자릿수 합

--- src/main/java/week6/day1107/Programmers12947.java
+++ src/main/java/week6/day1107/Programmers12947.java
@@ -3,6 +3,8 @@ package week6.day1107;
 public class Programmers12947 {
     public boolean solution(int x) {
 
+        int originX = x;
+
         // 자릿수 합
         int sumOfDigit = 0;
 
@@ -12,14 +14,25 @@ public class Programmers12947 {
         }
         System.out.println(sumOfDigit);
 
+//        // 자릿수 합 나누기
+//        boolean y = false;
+//        if (sumOfDigit%x==0) {
+//            System.out.println(sumOfDigit%x);
+//            return true;
+//        }
 
         // 자리수 합으로 나누어 지는지
-        return false;
+        return originX % sumOfDigit == 0;
     }
     public static void main(String[] args) {
 
         Programmers12947 pg = new Programmers12947();
         pg.solution(687);
+        System.out.println(pg.solution(687));
+        System.out.println(pg.solution(10));
+        System.out.println(pg.solution(12));
+        System.out.println(pg.solution(11));
+        System.out.println(pg.solution(13));
 
     }
 }
