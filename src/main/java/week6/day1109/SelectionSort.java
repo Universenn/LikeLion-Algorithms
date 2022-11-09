366a0c762a3b0e5c44f77d400a0588c04fcf021b
Universenn <dnwndhks98@gmail.com>
2022-11-09 10:05:14 +0900

    SelectionSort 내림차순 완료

--- src/main/java/week6/day1109/SelectionSort.java
+++ src/main/java/week6/day1109/SelectionSort.java
@@ -6,18 +6,18 @@ import java.util.Arrays;
 public class SelectionSort {
     public static void main(String[] args) {
         int[] arr = {2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
-        int minIdx = 0;
+        int maxIdx = 0;
         int temp = 0;
 
 
         for (int i = 0; i < arr.length; i++) {
-            minIdx = i;
+            maxIdx = i;
             for (int j = i; j < arr.length; j++) {
-                if (arr[minIdx] > arr[j]) minIdx = j;
+                if (arr[maxIdx] < arr[j]) maxIdx = j;
             }
             temp = arr[i];
-            arr[i] = arr[minIdx];
-            arr[minIdx] = temp;
+            arr[i] = arr[maxIdx];
+            arr[maxIdx] = temp;
 
             System.out.println(Arrays.toString(arr));
         }
