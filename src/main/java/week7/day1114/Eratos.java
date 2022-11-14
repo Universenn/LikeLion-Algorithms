a2d7cdd6776d130b152544f7144a9b39cb4abef7
Universenn <dnwndhks98@gmail.com>
2022-11-14 10:22:36 +0900

    Eratos 2를 제외한 2배수 0으로 변환

--- src/main/java/week7/day1114/Eratos.java
+++ src/main/java/week7/day1114/Eratos.java
@@ -8,8 +8,11 @@ public class Eratos {
     public static void main(String[] args) {
         int N = 50;
         int[] arr = new int[N-1];
+        // 2~50 나열
+        for (int i = 0; i < arr.length; i++) arr[i] = i+2;
+
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = i+2;
+            if (arr[i]!=2&&arr[i]%2==0) arr[i]=0;
         }
         System.out.println(Arrays.toString(arr));
 
