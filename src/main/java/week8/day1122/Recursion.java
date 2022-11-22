d863e5032ac8e86481371ae680a567aa7a4f6f4a
Universenn <dnwndhks98@gmail.com>
2022-11-22 09:20:34 +0900

    재귀 1-100까지 자기 자신 호출

--- src/main/java/week8/day1122/Recursion.java
+++ src/main/java/week8/day1122/Recursion.java
@@ -1,13 +1,17 @@
 package week8.day1122;
 
 public class Recursion {
-    public static void print1to100() {
-        for (int i = 1; i <= 100; i++) {
-            System.out.println(i);
+    public static void print1to100(int n) {
+//        for (int i = 1; i <= 100; i++) {
+//            System.out.println(i);
+//        }
+        if (n>100) {
+            return;
         }
-//        System.out.println(n);
+        System.out.println(n);
+        print1to100(n+1);
     }
     public static void main(String[] args) {
-        print1to100();
+        print1to100(1);
     }
 }
