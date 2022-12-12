9272f3363a8c0be48cded4f1864e2f5f33a796ed
Universenn <dnwndhks98@gmail.com>
2022-12-12 10:16:36 +0900

    나머지 구하기

--- src/main/java/week11/day1212/MinCostPath3.java
+++ src/main/java/week11/day1212/MinCostPath3.java
@@ -20,8 +20,13 @@ public class MinCostPath3 {
             for (int j = 0; j < matrix[0].length; j++) {
                 if (i == 0 && j == 0) {
                     dp[i][j] = matrix[i][j];
-                } else if (i > 0 && j == 0) {
+                }else if (i == 0 && j > 0) {
+                    dp[i][j] = dp[i][j - 1] + matrix[i][j];
+                }else if (i > 0 && j == 0) {
                     dp[i][j] = dp[i-1][j] + matrix[i][j];
+                }else if (i > 0 && j > 0) {
+                    // 위 i - 1, 왼쪽 j - 1, 대각선 위 [i-1][j-1]
+                    dp[i][j] = Math.min(Math.min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1])+matrix[i][j];
                 }
             }
         }
